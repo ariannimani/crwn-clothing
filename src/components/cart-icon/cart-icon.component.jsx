a82402781c33d53826956f9510091b2c630663ed
Arian N <aarriiaann@gmail.com>
2022-04-12 15:40:18 +0200

    update scss to styled components

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,6 +1,10 @@
 import React, { useContext } from "react";
-import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
-import { CartIconContainer, ItemCount } from "./cart-icon.styles.jsx";
+
+import {
+  CartIconContainer,
+  ItemCount,
+  ShoppingIcon,
+} from "./cart-icon.styles.jsx";
 import { CartContext } from "../../contexts/cart.context";
 
 const CartIcon = () => {
