a2cc98318200bcd8ebe383c7ee533ecddd22c304
Arian N <aarriiaann@gmail.com>
2022-04-12 15:53:40 +0200

    update redirects

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -1,6 +1,5 @@
 import { createContext, useState, useEffect } from "react";
 import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils.js";
-import SHOP_DATA from "./shopData.js";
 
 export const CategoriesContext = createContext({
   categories: [],
